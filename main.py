ef69aa8a15edc1b7c5c1d6a2f83be2dcd706dc96
Sourav <souravrao3110@gmail.com>
2021-02-18 21:14:25 +0530

    added

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from Detector import *
 
 detector = Detector(use_cuda=True)
 
-detector.processImage("input_data/download.png")
+detector.processImage("input_data/harry.jpg")
 #detector.processVideo(0)
