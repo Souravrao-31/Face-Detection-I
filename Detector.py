384a1d9361c91c0cad31d4ab54cb0f0e38bfe9bc
Sourav <souravrao3110@gmail.com>
2021-02-18 19:51:42 +0530

    set model for recog

--- Detector.py
+++ Detector.py
@@ -3,9 +3,13 @@ import cv2
 from imutils.video import FPS
 
 class Detector:
-    def __init__(self):
-        self.faceModel = cv2.dnn.readNetFromCaffe("model/res10_300x300_ssd_iter_140000.prototxt",
-        caffeModel ="model/res10_300x300_ssd_iter_140000.caffemodel")
+    def __init__(self, use_cuda= False):
+        self.faceModel = cv2.dnn.readNetFromCaffe("models/res10_300x300_ssd_iter_140000.prototxt",
+        caffeModel ="models/res10_300x300_ssd_iter_140000.caffemodel")
+        
+        if use_cuda:
+            self.facemodel.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+            self.facemodel.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
         
     def processImage(self, imgName):
         self.img = cv2.imread(imgName)
@@ -17,11 +21,38 @@ class Detector:
         cv2.waitKey(0)
         
     def processVideo(self, videoName):
-        cap = cv2
+        cap = cv2.VideoCapture(videoName)
+        if(cap.isOpen() == False):
+            print("Error opening video.........")
+            return
         
+        (sucess, self.img) = cap.read()
+        (self.height, self.width) = self.img.shape[:2]
+        
+        fps = FPS().start()
+        
+        while sucess:
+            self.processFrame()
+            cv2.imshow("Output", self.img)      
+            
+            key = cv2.waitKey(1) & 0xFF  
+            if key == ord("q"):
+                break
+            
+            fps.update()
+            (sucess, self.img) =cap.read()
+            
+        fps.stop()
+        print("Elapsed time: {:.2f}".format(fps.elapsed() ))   
+        print("FPS: {:.2f}".format(fps.fps())) 
+        
+        cap.release()
+        cv2.destroyAllWindows()
+                        
     def processFrame(self):
         blob = cv2.dnn.blobFromImage(self.img, 1.0, (300,300), (104.0, 177.0, 123.0), 
-               swapRB = False, crop = False)
+        swapRB = False, crop = False)
+        
         self.faceModel.setInput(blob)
         predictions = self.faceModel.forward()
         
